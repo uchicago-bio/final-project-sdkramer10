8ccbed7653dc1ce6de76d2cec8b2bd58f307e6bf
Scott Kramer <scottkramer@Scotts-MacBook-Pro.local>
2016-01-30 09:41:39 -0600

    created headers file

--- code/SomaticMutationClassifier.py
+++ code/SomaticMutationClassifier.py
@@ -34,18 +34,57 @@ class SomaticMutationClassifier:
         
         return predictions, callersInPipeline
 
-svnFiles = ['SNVCalls_IS1.txt', 'SNVCalls_IS2.txt', 'SNVCalls_IS3.txt', 'SNVCalls_IS4.txt']
+svnFiles = [
+    'SNVCalls_IS1.txt', 
+    'SNVCalls_IS2.txt', 
+    'SNVCalls_IS3.txt', 
+    'SNVCalls_IS4.txt', 
+    'SNVCalls_CPCG0100.txt', 
+    'SNVCalls_CPCG0183.txt',
+    'SNVCalls_CPCG0184.txt',
+    'SNVCalls_CPCG0196.txt',
+    'SNVCalls_CPCG0235.txt',
+    'SNVCalls_PCSI0023.txt',
+    'SNVCalls_PCSI0044.txt',
+    'SNVCalls_PCSI0046.txt',
+    'SNVCalls_PCSI0048.txt',
+    'SNVCalls_PCSI0072.txt'
+]
+
 truthFiles = ['synthetic.challenge.set1.tumor.all.truth.vcf', 'synthetic.challenge.set2.tumor.all.truth.vcf', 'synthetic.challenge.set3.tumor.20pctmasked.truth.vcf', 'synthetic.challenge.set4.tumour.25pctmasked.truth.vcf']
 
+callerList = list()
 for i in range(len(svnFiles)):
     svnCalls = pd.read_csv(svnFiles[i], delimiter='\t', dtype={'CHROM':pd.np.str})
-    truths = TumorTruths(truthFiles[i])
+    #truths = TumorTruths(truthFiles[i])
 
-    resultsArray = truths.GetTruths(svnCalls)
+    #resultsArray = truths.GetTruths(svnCalls)
     svnCalls = svnCalls[svnCalls.columns[3:-1]]
     
     somaticMutationClassifier = SomaticMutationClassifier(num_clusters=5, percentile=0.40)
     somaticMutationClassifier.fit(svnCalls)
     predictions, callersInPipeline = somaticMutationClassifier.predict(svnCalls)
-    print f1_score(predictions, resultsArray)
-    print callersInPipeline
+   # print f1_score(predictions, resultsArray)
+    callerList.append(callersInPipeline)
+
+
+headers = [
+    '##synthetic_1_Pipelines',
+    '##synthetic_2_Pipelines',
+    '##synthetic_3_Pipelines',
+    '##synthetic_4_Pipelines',
+    '##CPCG0100_Pipelines',
+    '##CPCG0183_Pipelines',
+    '##CPCG0184_Pipelines',
+    '##CPCG0196_Pipelines',
+    '##CPCG0235_Pipelines',
+    '##PCSI0023_Pipelines',
+    '##PCSI0044_Pipelines',
+    '##PCSI0046_Pipelines',
+    '##PCSI0048_Pipelines',
+    '##PCSI0072_Pipelines'
+]
+
+for i in range(len(callerList)):
+    callers = ','.join(callerList[i])
+    print '%s=%s' % (headers[i], callers)
